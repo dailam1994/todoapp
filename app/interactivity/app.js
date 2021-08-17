0b95b9f4fa23b976f34754538fed2bb1fde0dfd3
dailam1994 <dai_lam88@hotmail.com>
2021-08-18 08:33:16 +1000

    ToDo App v1.1

--- app/interactivity/app.js
+++ app/interactivity/app.js
@@ -1,1 +1,203 @@
 console.log('ToDo App');
+// Login
+function login(e) {
+    e.preventDefault();
+    console.log(e);
+    var theFormData = {};
+    for (i=0;i<e.target.length;i++) {
+        theFormData[e.target[i].name] =
+            e.target[i].value;
+    }
+    fetch(e.target.action, 
+        {
+            method: 'POST',
+            body: JSON.stringify(theFormData),
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    );
+}
+
+function register(e) {
+    e.preventDefault();
+    // console.log(e);
+    var theFormData = {};
+    for (i=0;i<e.target.length;i++) {
+        theFormData[e.target[i].name] =
+            e.target[i].value;
+    }
+    fetch(e.target.action,
+        {
+            method: 'PUT',
+            body: JSON.stringify(theFormData),
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    );
+}
+
+function userexists(e) {
+    e.preventDefault();
+    // console.log(e);
+    url = e.target.action + '?username=' + 
+        e.target[0].value;
+    fetch(url, 
+        {
+            method: 'GET',
+            credentials: 'include'
+        }
+    );
+}
+
+function userloggedin(e) {
+    e.preventDefault();
+    // console.log(e);
+    fetch(e.target.action,
+        {
+            method: 'GET',
+            credentials: 'include'
+        }
+    );
+}
+
+function userlogout(e) {
+    e.preventDefault();
+    // console.log(e);
+    fetch(e.target.action,
+        {
+            method: 'GET',
+            credentials: 'include'
+        }
+    );
+}
+
+// Categories
+function getcategories(e) {
+    e.preventDefault();
+    // console.log(e);
+    fetch(e.target.action,
+        {
+            method: 'GET',
+            credentials: 'include'
+        }
+    );
+}
+
+function addcategory(e) {
+    e.preventDefault();
+    // console.log(e);
+    var theFormData = {};
+    for (i=0;i<e.target.length;i++) {
+        theFormData[e.target[0].name] =
+            e.target[0].value;
+    }
+    fetch(e.target.action, 
+        {
+            method: 'PUT',
+            body: JSON.stringify(theFormData),
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    );
+}
+
+function delcategory(e) {
+    e.preventDefault();
+    // console.log(e);
+    var url = e.target.action + '/' + e.target[0].value; 
+    fetch(url, 
+        {
+            method: 'DELETE',
+            credentials: 'include'
+        }
+    );
+}
+
+function updatecategory(e) {
+    e.preventDefault();
+    // console.log(e);
+    var url = e.target.action + '/' + e.target[0].value;
+    var theFormData = {};
+    theFormData[e.target[1].name] = 
+        e.target[1].value;
+    fetch(url, 
+        {
+            method: 'PATCH',
+            body: JSON.stringify(theFormData),
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    );
+}
+
+// Items
+function gettodos(e) {
+    e.preventDefault();
+    // console.log(e);
+    var url = e.target.action + '/' + e.target[0].value;
+    fetch(url,
+        {
+            method: 'GET',
+            credentials: 'include'
+        }
+    );
+}
+
+function addtodo(e) {
+    e.preventDefault();
+    // console.log(e);
+    var theFormData = {};
+    for (i=0;i<e.target.length;i++) {
+        theFormData[e.target[0].name] =
+            e.target[0].value;
+    }
+    fetch(e.target.action, 
+        {
+            method: 'PUT',
+            body: JSON.stringify(theFormData),
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    );
+}
+
+function deltodo(e) {
+    e.preventDefault();
+    // console.log(e);
+    var url = e.target.action + '/' + e.target[0].value; 
+    fetch(url, 
+        {
+            method: 'DELETE',
+            credentials: 'include'
+        }
+    );
+}
+
+function updatetodo(e) {
+    e.preventDefault();
+    // console.log(e);
+    var url = e.target.action + '/' + e.target[0].value;
+    var theFormData = {};
+    theFormData[e.target[1].name] = 
+        e.target[1].value;
+    fetch(url, 
+        {
+            method: 'PATCH',
+            body: JSON.stringify(theFormData),
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    );
+}
--- server.js
+++ server.js
@@ -3,8 +3,64 @@ const app = express()
 
 app.use('/', express.static('app'))
 
+// Authentication
 app.get('/api', function (req, res) {
     res.send('Test API')
 })
+
+app.post('/api/login', function (req, res) {
+    res.send('Test login')
+})
+
+app.put('/api/register', function (req, res) {
+    res.send('Test register')
+})
+
+app.get('/api/doesuserexist', function (req, res) {
+    res.send('Test userexists')
+})
+
+app.get('/api/isloggedin', function (req, res) {
+    res.send('Test isLoggedin')
+})
+
+app.get('/api/logout', function (req, res) {
+    res.send('Test Logout')
+})
+
+// ToDo Categories CRUD
+app.get('/api/getcat', function (req, res) {
+    res.send('Test get categories')
+})
+
+app.put('/api/addcat', function (req, res) {
+    res.send('Test add categories')
+})
+
+app.delete('/api/delcat/:catid', function (req, res) {
+    res.send('Test delete categories')
+})
+
+app.patch('/api/updatecat/:catid', function (req, res) {
+    res.send('Test update categories')
+})
+
+// ToDo List CRUD
+app.get('/api/gettodos/:catid', function (req, res) {
+    res.send('Test Get ToDo')
+})
+
+app.put('/api/addtodo', function (req, res) {
+    res.send('Test Add ToDo')
+})
+
+app.delete('/api/deltodo/:todoid', function (req, res) {
+    res.send('Test Delete ToDo')
+})
+
+app.patch('/api/updatetodo/:todoid', function (req, res) {
+    res.send('Test Update ToDo')
+})
+
 
 app.listen(3000)
